c4d75d0358317ffd2ecc383e398891b4ac303f40
rbooker <ryan.m.booker@gmail.com>
2021-08-01 13:03:06 -0700

    Add files via upload

---
+++ step1.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const process = require('process');
+
+function cat (path) {
+    fs.readFile(path, "utf8", (err, data) => {
+        
+        if (err){
+            console.log(`Error reading ${path}:`)
+            console.log(`\t${err}`);
+            process.exit(1);
+        }
+        
+        console.log(data);
+    })
+}
+
+cat(process.argv[2]);
